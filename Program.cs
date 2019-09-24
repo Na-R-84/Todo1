0f500caac51f81d457086125e950fcc51d872fef
Nasim <nasim.roshan84@gmail.com>
2019-09-24 13:36:13 +0200

    Add Id and Completed

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using ToDo.domain;
 
 namespace ToDo
 {
     class Program
     {
+        
+        static Task[] taskList = new Task[100];
+
         static void Main(string[] args)
         {
-            
+
             bool shouldRun = true;
+            int taskIdCounter = 1;
             while (shouldRun)
             {
                 Console.Clear();
@@ -24,15 +29,69 @@ namespace ToDo
                 {
 
                     case ConsoleKey.D1:
+                        Console.Write("Title :");
+                        string title = Console.ReadLine();
+
+                        Console.Write("Due Time (yyyy-mm-dd hh:mm):");
+                        DateTime dueTime = DateTime.Parse(Console.ReadLine());
+
+                        taskList[GetIndexPosition()] = new Task(taskIdCounter, title, dueTime);
+                        break;
+
+
+                    case ConsoleKey.D2:
+
+                        Console.WriteLine("ID         Title           Due date            Completed     ");
+                        Console.WriteLine("----------------------------------------------------------");
+
+                        foreach (var task in taskList)
+                        {
+                            if (task == null) continue;
+                            Console.WriteLine($"{task.Id} {task.Title}{task.DueDate.ToString().PadLeft(25, ' ')}");
+                        }
+                        Console.ReadKey(true);
+
+                        //Console.WriteLine();
+                        //Console.WriteLine("(M)ark as completed");
+                        //keypressed = Console.ReadKey(true);
+                        //switch (keypressed.Key)
+                        //{
+                        //    case ConsoleKey.M:
+
+                        //        break;
+
+                        //}
 
                         break;
 
                     case ConsoleKey.D3:
 
                         shouldRun = false;
+
                         break;
                 }
             }
+        }
+        static int GetIndexPosition()
+        {
+            int result = -1;
+            for (int i = 0; i < taskList.Length; i++)
+            {
+                if (taskList[i] != null)
+                {
+                    continue;
+                }
+                if (taskList[i] == null)
+                {
+                    result = i;
+                    break;
+                }
+                if (result == -1)
+                {
+                    throw new Exception("No avalible position");
+                }
+            }
+            return result;
         }
     }
 }
---
+++ domain/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.domain
+{
+    class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? Completed { get; set; }
+        //public bool IsCompleted
+        //{
+        //    get
+        //    {
+        //        return Completed != null;
+        //    }
+        //}
+
+        //string title;  vi behöver inte de mer på grund av dem redan står där uppe
+        //DateTime dueDate;
+
+
+        public Task(int id,string title,DateTime dueDate)
+        {
+            Id = id;
+            Title = title;
+            DueDate = dueDate;
+        }
+    }
+}
