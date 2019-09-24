f41154bb5ae20328b2ae83fa1f4be943403ffc04
Nasim <nasim.roshan84@gmail.com>
2019-09-24 09:39:16 +0200

    add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace ToDo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello ");
+            bool shouldRun = true;
+            while (shouldRun)
+            {
+                Console.Clear();
+
+                Console.WriteLine("1. add todo");
+                Console.WriteLine("2. list todo");
+                Console.WriteLine("3. Exit");
+
+                ConsoleKeyInfo keypressed = Console.ReadKey(true);
+
+                switch (keypressed.Key)
+                {
+
+                    case ConsoleKey.D1:
+
+                        break;
+                }
+            }
         }
     }
 }
